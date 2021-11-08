b206823895f6c48f5a242ff1b7aa628ca74d8e00
maikl <michael.peer@inode.at>
2021-11-08 16:53:05 +0100

    Changed format and comments

--- Raspberry_Pi_Webserver/statusControl.py
+++ Raspberry_Pi_Webserver/statusControl.py
@@ -3,14 +3,17 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+
 # define sensors GPIOs
 button = 20
 senPIR = 16
+
 # define actuators GPIOs
 wire1 = 6
 wire2 = 13
 wire3 = 19
 wire4 = 26
+
 # initialize GPIO status variables
 buttonSts = 0
 senPIRSts = 0
@@ -18,15 +21,18 @@ wire1Sts = 0
 wire2Sts = 0
 wire3Sts = 0
 wire4Sts = 0
+
 # Define button and PIR sensor pins as an input
 GPIO.setup(button, GPIO.IN)
 GPIO.setup(senPIR, GPIO.IN)
-# Define led pins as output
+
+# Define wire pins as output
 GPIO.setup(wire1, GPIO.OUT)
 GPIO.setup(wire2, GPIO.OUT)
 GPIO.setup(wire3, GPIO.OUT)
 GPIO.setup(wire4, GPIO.OUT)
-# turn leds OFF
+
+# turn wires OFF
 GPIO.output(wire1, GPIO.LOW)
 GPIO.output(wire2, GPIO.LOW)
 GPIO.output(wire3, GPIO.LOW)
@@ -35,6 +41,7 @@ GPIO.output(wire4, GPIO.LOW)
 
 @app.route("/")
 def index():
+
     # Read GPIO Status
     buttonSts = GPIO.input(button)
     senPIRSts = GPIO.input(senPIR)
