c65afda952b661809b7afd15df9f2e0d25ff2f0e
pr-michael <michael.peer@inode.at>
2021-12-12 20:38:42 +0100

    Added function to control cable 2

--- ESP32/webserver_gpio/src/main.cpp
+++ ESP32/webserver_gpio/src/main.cpp
@@ -7,9 +7,11 @@ const char *ssid = "ssid";
 const char *password = "passwd";
 
 // Set LED GPIO
-const int ledPin = 2;
+const int ledPin1 = 2;
+const int ledPin2 = 4;
 // Stores LED state
-String ledState;
+String ledState1;
+String ledState2;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -17,19 +19,31 @@ AsyncWebServer server(80);
 // Replaces placeholder with LED state value
 String processor(const String &var)
 {
-  Serial.println(var);
-  if (var == "STATE")
+  if (var == "STATE1")
   {
-    if (digitalRead(ledPin))
+    if (digitalRead(ledPin1))
     {
-      ledState = "ON";
+      ledState1 = "ON";
     }
     else
     {
-      ledState = "OFF";
+      ledState1 = "OFF";
     }
-    Serial.print(ledState + "\n");
-    return ledState;
+    Serial.print(ledState1 + "\n");
+    return ledState1;
+  }
+  else if (var == "STATE2")
+  {
+    if (digitalRead(ledPin2))
+    {
+      ledState2 = "ON";
+    }
+    else
+    {
+      ledState2 = "OFF";
+    }
+    Serial.print(ledState2 + "\n");
+    return ledState2;
   }
   return String();
 }
@@ -38,7 +52,8 @@ void setup()
 {
   // Serial port for debugging purposes
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 
   // Initialize SPIFFS
   if (!SPIFFS.begin(true))
@@ -70,17 +85,31 @@ void setup()
   server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send(SPIFFS, "/style.css", "text/css"); });
 
-  // Route to set GPIO to HIGH
-  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request)
+  // Route to set cable1 to HIGH
+  server.on("/on1", HTTP_GET, [](AsyncWebServerRequest *request)
             {
-              digitalWrite(ledPin, HIGH);
+              digitalWrite(ledPin1, HIGH);
               request->send(SPIFFS, "/index.html", String(), false, processor);
             });
 
-  // Route to set GPIO to LOW
-  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request)
+  // Route to set cable1 to LOW
+  server.on("/off1", HTTP_GET, [](AsyncWebServerRequest *request)
             {
-              digitalWrite(ledPin, LOW);
+              digitalWrite(ledPin1, LOW);
+              request->send(SPIFFS, "/index.html", String(), false, processor);
+            });
+
+  // Route to set cable2 to HIGH
+  server.on("/on2", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              digitalWrite(ledPin2, HIGH);
+              request->send(SPIFFS, "/index.html", String(), false, processor);
+            });
+
+  // Route to set cable2 to LOW
+  server.on("/off2", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              digitalWrite(ledPin2, LOW);
               request->send(SPIFFS, "/index.html", String(), false, processor);
             });
 
